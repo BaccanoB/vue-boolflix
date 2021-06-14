6b40a9f3938cd5ec9749f6ea187138d8d36e9d66
BaccanoB <beatrice.baccano@yahoo.it>
2021-06-14 11:54:21 +0200

    collegata api

---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name:'Main',
+    created: function(){
+        axios
+           .get('https://api.themoviedb.org/3/search/movie',{
+               params:{
+                   api_key: '838b4a196c57a8cffba225213ba6f953',
+                   query: String,
+                   language: 'it-IT'
+                }
+            })
+            .then(
+                (res) =>{
+                    console.log(res);
+                }
+            )
+    }
+}
+
+</script>
+
+<style>
+
+</style>
